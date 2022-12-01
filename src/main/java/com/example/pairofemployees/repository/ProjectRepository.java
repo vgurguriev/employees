00902d1355718faf4686ca964e0bb86bbf2df2b7
vgurguriev <v.gurguriev@gmail.com>
2022-12-01 17:57:27 +0200

    Initial commit

--- src/main/java/com/example/pairofemployees/repository/ProjectRepository.java
+++ src/main/java/com/example/pairofemployees/repository/ProjectRepository.java
@@ -3,12 +3,9 @@ package com.example.pairofemployees.repository;
 import com.example.pairofemployees.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     Optional<Project> findFirstByOrderByTotalTimeDesc();
-
 }
